44b1c6e803042e2b10a7ed5a00a752440a885a91
Mark Luu <thatoneguy1055@yahoo.ca>
2017-07-13 20:38:34 -0600

    ARE YOU READY KIDS? AYE AYE KAPTN!

---
+++ python/hangee mano.py
@@ -0,0 +1,46 @@
+import random
+
+words = [
+    "well",
+    "okay",
+    "guess",
+    "sort",
+    "maybe",
+    "why",
+    "never",
+    "cannot",
+    "why",
+    "smelly",
+    "blanks",
+    "north",
+    "candy",
+    "wonder",
+    "and",
+    "not",
+    "man",
+    "stop"
+
+]
+
+random.shuffle(words)
+word = words[0]
+solution = "_" * len(word)
+tries = 7 * len(word)
+
+while word != solution and tries != 0:
+    print(solution)
+    guess = input("Guess a letter: ")
+
+
+    for i in range(0, len(word)):
+        if word[i] == guess:
+            solution = list(solution)
+            solution[i] = guess
+            solution = "".join(solution)
+        else:
+            tries = tries - 1
+
+        if tries == 0:
+            print("Jokes.", word)
+
+print("The word was", word)
---
+++ python/i'm dead.py
@@ -0,0 +1,10 @@
+name = input("Prove to me you don't smell.")
+
+if name == "i'm dead":
+    print("Close enough. You're dead, right?")
+name != "i'm dead"
+name = input("Seriously, are you dead?")
+if name == ("yes"):
+    print("Welcome to my circle of hell. You win.")
+else:
+    print("Go away, then.")
